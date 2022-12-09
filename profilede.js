3d9206d8c8f27d4bfd5235bda60ee69055ea3621
sekikhsadi <120183222+sekikhsadi@users.noreply.github.com>
2022-12-09 18:53:28 +0600

    hoise

---
+++ profilede.js
@@ -0,0 +1,6 @@
+document.getElementById("butsub").addEventListener("click",addedToCricbuzz)
+
+function addedToCricbuzz(){
+    alert("Profile added to cricbuzz")
+    window.location.href="index.html"
+}
